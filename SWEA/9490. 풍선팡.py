02cf76ad96d01c1804a6419045141f382430fbc1
이유진 <yjoct13@gmail.com>
2023-08-12 13:26:28 +0900

    [SWEA] 9490. 풍선팡

---
+++ SWEA/9490. 풍선팡.py
@@ -0,0 +1,23 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    dr = [1, 0, -1, 0]  # 우, 상, 좌, 하
+    dc = [0, 1, 0, -1]
+
+    max_v = 0
+
+    for r in range(N):
+        for c in range(M):
+            sum_v = 0
+            for k in range(4):
+                for s in range(1, arr[r][c] + 1):
+                    nr = r + (dr[k] * s)
+                    nc = c + (dc[k] * s)
+                    if 0 <= nr < N and 0 <= nc < M:
+                        sum_v += arr[nr][nc]
+            if max_v < sum_v + arr[r][c]:
+                max_v = sum_v + arr[r][c]
+
+    print(f'#{tc} {max_v}')
