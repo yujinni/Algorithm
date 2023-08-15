5b2faca350d2db89a967a7af0d0dc3594ff701e3
이유진 <yjoct13@gmail.com>
2023-08-15 23:21:58 +0900

    [SWEA] 16530. Forth

---
+++ SWEA/16530. Forth.py
@@ -0,0 +1,32 @@
+T = int(input())
+for tc in range(1, T+1):
+    arr = list(input().split())
+    stack = []
+
+    for i in arr:
+        if i in '+-*/' and len(stack) < 2:
+            print(f'#{tc} error')
+            break
+        elif i in '+-*/' and len(stack) >= 2:
+            b = stack.pop()
+            a = stack.pop()
+            if i == '+':
+                stack.append(a + b)
+            elif i == '-':
+                stack.append(a - b)
+            elif i == '*':
+                stack.append(a * b)
+            elif i == '/':
+                stack.append(a // b)
+        elif i == '.':
+            pass
+        else:  # 숫자인 경우
+            stack.append(int(i))
+      
+    else:
+      if len(stack) == 1:
+          print(f'#{tc} {stack[-1]}')
+      else:
+          print(f'#{tc} error')
+      
+            
