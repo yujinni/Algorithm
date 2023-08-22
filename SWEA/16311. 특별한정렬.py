aedff60661aebb1a38d55eb41ee8bd422d280192
이유진 <yjoct13@gmail.com>
2023-08-23 00:34:40 +0900

    [SWEA] 16311. 특별한정렬 주석 추가

--- SWEA/16311. 특별한정렬.py
+++ SWEA/16311. 특별한정렬.py
@@ -5,10 +5,11 @@ for tc in range(1, T + 1):
 
     for i in range(N):
         for j in range(i + 1, N):
-            if i % 2 == 0:
+            # 해당 인덱스를 그 뒤의 모든 인덱스와 비교
+            if i % 2 == 0:  # 짝수 번째 인덱스에는 큰 값부터 내림차순 정렬되도록
                 if num_list[j] > num_list[i]:
                     num_list[j], num_list[i] = num_list[i], num_list[j]
-            else:
+            else:  # 홀수 번째 인덱스에는 작은 값부터 오름차순 정렬되도록
                 if num_list[j] < num_list[i]:
                     num_list[j], num_list[i] = num_list[i], num_list[j]
     print(f'#{tc}', *num_list[:10])
