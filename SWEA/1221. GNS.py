98b21bfc6d03f0e96b462f5bbf8d73d6042d8fe6
yujinni <yjoct13@gmail.com>
2023-08-23 17:11:15 +0900

    [SWEA] 1221. GNS 수정

--- SWEA/1221. GNS.py
+++ SWEA/1221. GNS.py
@@ -1,15 +1,15 @@
 T = int(input())
-arr = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
+arr = ['ZRO', 'ONE', 'TWO', 'THR', 'FOR', 'FIV', 'SIX', 'SVN', 'EGT', 'NIN']
 
 for tc in range(1, T+1):
-    n = list(map(str, input().split()))
-    number_list = list(map(str, input().split()))
+    N = list(map(str, input().split()))
+    num_list = list(map(str, input().split()))
     ans = []
 
-    for i in range(10):
-        for j in number_list:
-            if arr[i] == j:
-                ans.append(j)
+    for i in arr:
+        for num in num_list:
+            if i == num:
+                ans.append(i)
 
-    print(n[0])
+    print(N[0])
     print(*ans)
