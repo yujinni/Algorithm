22d3e159e5dffcbb9934ae0e17b05e24df8336cd
yujinni <yjoct13@gmail.com>
2023-08-29 15:44:09 +0900

    [SWEA] 16260. 색칠하기 변수명 수정

--- SWEA/16260. 색칠하기.py
+++ SWEA/16260. 색칠하기.py
@@ -1,16 +1,16 @@
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    arr = [[0] * 10 for _ in range(10)]
-    purple = 0
+    grid = [[0] * 10 for _ in range(10)]
+    cnt = 0
 
     for _ in range(N):
         r1, c1, r2, c2, color = map(int, input().split())
         for r in range(r1, r2+1):
             for c in range(c1, c2+1):
-                if arr[r][c] == 0:
-                    arr[r][c] = color
+                if grid[r][c] == 0:
+                    grid[r][c] = color
                 else:
-                    purple += 1
+                    cnt += 1
                     
-    print(f'#{tc} {purple}')
+    print(f'#{tc} {cnt}')
