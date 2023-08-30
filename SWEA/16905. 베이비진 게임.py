c4829ff7497e045f6c85f57a0008eedc85565185
이유진 <yjoct13@gmail.com>
2023-08-31 00:10:55 +0900

    [SWEA] 16905. 베이비진 게임 주석 추가

--- SWEA/16905. 베이비진 게임.py
+++ SWEA/16905. 베이비진 게임.py
@@ -1,10 +1,10 @@
 def baby_gin(cnts, idx):
     if cnts[idx] == 3:
         return 1
-    for i in (idx-2, idx-1, idx):
-        if 0 <= i <= 7 and cnts[i] > 0 and cnts[i+1] > 0 and cnts[i+2] > 0:
+    for i in (idx-2, idx-1, idx):  # run인 경우
+        if 0 <= i <= 7 and cnts[i] > 0 and cnts[i+1] > 0 and cnts[i+2] > 0:  # 유효한 숫자 카드(0부터 9까지) 범위 내에 있는지 여부
             return 1
-    return 0
+    return 0  # run 또는 triplet이 아닌 경우
 
 
 T = int(input())
@@ -14,12 +14,12 @@ for tc in range(1, T + 1):
     cnts1 = [0]*10
     cnts2 = [0]*10
 
-    ans = 0
+    ans = 0  # break를 하지 않은 경우 무승부로 0 출력
 
     for i in range(len(arr)):
-        if i % 2 == 0:
-            cnts1[arr[i]] += 1
-            if baby_gin(cnts1, arr[i]):
+        if i % 2 == 0:  # 짝수인 경우
+            cnts1[arr[i]] += 1  # 플레이어 1에게 추가
+            if baby_gin(cnts1, arr[i]):  # 베이비진인지 확인했을 때 true인 경우
                 ans = 1
                 break
         else:
