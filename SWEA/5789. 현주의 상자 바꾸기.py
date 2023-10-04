03a7294a7358b485cee15fc15539fa16d955a6ba
이유진 <yjoct13@gmail.com>
2023-10-04 21:50:57 +0900

    [SWEA] 5789. 현주의 상자 바꾸기

---
+++ SWEA/5789. 현주의 상자 바꾸기.py
@@ -0,0 +1,12 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N, Q = map(int, input().split())
+    arr = [0] * N
+ 
+    for i in range(1, Q + 1):
+        L, R = map(int, input().split())
+        arr[L-1:R] = [i] * (R - L + 1)
+ 
+    ans = ' '.join(map(str, arr))
+ 
+    print(f'#{tc} {ans}')
