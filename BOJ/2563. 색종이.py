d41f60801f00bd38ae22d7056f6561718cd7faf2
이유진 <yjoct13@gmail.com>
2023-10-09 20:40:29 +0900

    [BOJ] 2563. 색종이

--- BOJ/2563. 색종이.py
+++ BOJ/2563. 색종이.py
@@ -3,7 +3,7 @@ paper = [[0] * 100 for _ in range(100)]
 cnt = 0
 
 for _ in range(N):
-    x, y = list(map(int, input().split()))
+    x, y = map(int, input().split())
 
     for r in range(y, y + 10):
         for c in range(x, x + 10):
